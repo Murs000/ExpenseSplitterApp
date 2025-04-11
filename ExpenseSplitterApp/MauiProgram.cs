74c9e1101c2375d1d5236e5881eb77fec16e73b9
Murs000 <vizortele096@gmail.com>
2025-04-11 16:32:10 +0400

    App. Update MainPage View.

--- ExpenseSplitterApp/MauiProgram.cs
+++ ExpenseSplitterApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using ExpenseSplitterApp.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace ExpenseSplitterApp
 {
@@ -15,8 +16,10 @@ namespace ExpenseSplitterApp
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton<MainPageViewModel>();
+            builder.Services.AddSingleton<MainPage>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- ExpenseSplitterApp/Views/MainPage.xaml.cs
+++ ExpenseSplitterApp/Views/MainPage.xaml.cs
@@ -1,25 +1,20 @@
-﻿namespace ExpenseSplitterApp
+﻿using ExpenseSplitterApp.ViewModels;
+using ExpenseSplitterApp.Views;
+
+namespace ExpenseSplitterApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage()
+        private readonly MainPageViewModel _viewModel;
+        public MainPage(MainPageViewModel viewModel)
         {
             InitializeComponent();
+            _viewModel = viewModel;
+            BindingContext = viewModel;
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnGoToSecondPageClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushAsync(new CreatePersonPage(_viewModel));
         }
     }
-
 }
