505b07ca340a9719f9ce95db3f467002ee4c18bc
Murs000 <vizortele096@gmail.com>
2025-04-11 16:31:26 +0400

    App. Add Models.

---
+++ ExpenseSplitterApp/Models/ExpenceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseSplitterApp.Models
+{
+    public class ExpenceModel
+    {
+        public int Id { get; set; }
+        public double ExpenceAmount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int PersonId { get; set; }
+    }
+}
