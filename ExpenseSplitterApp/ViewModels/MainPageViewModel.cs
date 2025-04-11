509f0c7f8b247d798ec4b48d7ea0ab5b5d6e58bc
Murs000 <vizortele096@gmail.com>
2025-04-11 16:31:45 +0400

    App. Update ViewModel.

--- ExpenseSplitterApp/ViewModels/MainPageViewModel.cs
+++ ExpenseSplitterApp/ViewModels/MainPageViewModel.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using ExpenseSplitterApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExpenseSplitterApp.ViewModels
 {
-    class MainPageViewModel
+    public class MainPageViewModel
     {
+        public ObservableCollection<PersonModel> Persons { get; set; }
+        public ObservableCollection<ExpenceModel> Expences { get; set; }
 
+        public MainPageViewModel() 
+        {
+            Persons = new ObservableCollection<PersonModel>
+            {
+                new PersonModel { Id = 1, Name ="1"},
+                new PersonModel { Id = 2, Name ="2"},
+                new PersonModel { Id = 3, Name ="3"},
+                new PersonModel { Id = 4, Name ="4"}
+            };
+            Expences = new ObservableCollection<ExpenceModel>
+            {
+                new ExpenceModel { Id = 1, Description = "Food", ExpenceAmount = 14.99, PersonId = 1 },
+                new ExpenceModel { Id = 1, Description = "Food", ExpenceAmount = 17.98, PersonId = 1 },
+                new ExpenceModel { Id = 1, Description = "Food", ExpenceAmount = 13.30, PersonId = 1 },
+                new ExpenceModel { Id = 1, Description = "Food", ExpenceAmount = 14.99, PersonId = 2 }
+            };
+        }
     }
 }
